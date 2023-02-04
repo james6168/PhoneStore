2603432d1e8a3cd24035b187962feac0c57e2b9a
james6168 <95910014+james6168@users.noreply.github.com>
2023-02-05 01:49:19 +0600

    feat: Register API view was implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from accounts.views import LoginAPIView
+from accounts.views import *
 urlpatterns = [
-    path('accounts/login/', LoginAPIView.as_view())
+    path('accounts/login/', LoginAPIView.as_view()),
+    path('accounts/register/', RegisterAPIView.as_view())
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,11 @@ from django.http import JsonResponse
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
 from django.core.serializers import serialize
 from http import HTTPStatus
+import re
 from django.contrib.auth import authenticate
 import json
 # Create your views here.
@@ -28,5 +30,38 @@ class LoginAPIView(View):
                 return JsonResponse(response_data, status=HTTPStatus.OK)
             return JsonResponse({"error": "Incorrect password"}, status=HTTPStatus.BAD_REQUEST)
         return JsonResponse({"error": "Such user does not exist"}, status=HTTPStatus.BAD_REQUEST)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class RegisterAPIView(View):
+
+    def post(self, request):
+        allowed_parameters = {"email", "password1", "password2"}
+        request_data = json.loads(request.body)
+        for key in request_data:
+            if key not in allowed_parameters:
+                return JsonResponse({"status": "error",
+                                     "error_message": "Extra parameters are not allowed"},
+                                    status=HTTPStatus.BAD_REQUEST)
+        email = request_data.get("email")
+        password1 = request_data.get("password1")
+        password2 = request_data.get("password2")
+
+        if re.fullmatch(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b", email):
+            if validate_password(password1) is not None:
+                if password1 == password2:
+                    user = User.objects.create(**request_data)
+                    user.save()
+                    return JsonResponse({"status": "success",
+                                         "email": email},
+                                        status=HTTPStatus.CREATED)
+                return JsonResponse({"status": "error",
+                                     "error_message": "Passwords mismatch"},
+                                    status=HTTPStatus.BAD_REQUEST)
+            return JsonResponse({"status": "error",
+                                 "error_message": "Password should have at least 8 symbols, numeric symbols, upper- "
+                                                  "and lowercase symbols"},
+                                status=HTTPStatus.BAD_REQUEST)
+
 
 
