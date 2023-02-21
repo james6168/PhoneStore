24103c3ad9e5eeb7a9684ab046ebbd1bf48f38a6
james6168 <95910014+james6168@users.noreply.github.com>
2023-02-21 18:48:34 +0600

    feat: pagination feature for Smartphone API view was implemented

--- smartphones/views.py
+++ smartphones/views.py
@@ -12,10 +12,10 @@ class SmartphoneListAPIView(View):
     global allowed_parameters
     allowed_parameters = {"page", "search"}
 
-
     def get(self, request):
         query_dict = request.GET
         search_parameter = query_dict.get("search")
+        page_parameter = int(query_dict.get("page"))
         for each_key in query_dict:
             if each_key not in allowed_parameters:
                 return JsonResponse({"status": "error",
@@ -25,6 +25,16 @@ class SmartphoneListAPIView(View):
         smartphones_query_set = Smartphone.objects.all()
 
         if query_dict.get("search") == None:
+            elements_sublists = [smartphones_query_set[x: x + 2] for x in range(0, len(smartphones_query_set), 2)]
+            pages_count = len(elements_sublists)
+            print(elements_sublists)
+            print(page_parameter)
+
+            if page_parameter is not None:
+                return JsonResponse({"status": "success",
+                                     "smartphones": serialize("python", elements_sublists[page_parameter])},
+                                    status=HTTPStatus.OK)
+
             return JsonResponse({"status": "success",
                                  "smartphones": serialize("python", smartphones_query_set)},
                                  status=HTTPStatus.OK)
